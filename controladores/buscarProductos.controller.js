d4b8ce7a4c9c7b8345e6bfefc832a92ebd0446ec
Rodrigo Saforcada <ronninn18@gmail.com>
2022-08-14 16:09:44 -0300

    CRUD: avances en la funcionalidad y estilos de la barra de busqueda

--- controladores/buscarProductos.controller.js
+++ controladores/buscarProductos.controller.js
@@ -2,21 +2,41 @@ import { clientServices } from "../servicio/client-service.js";
 
 const inputBusqueda = document.querySelector(".barra_de_busqueda__ingresar_solicitud");
 
+const resultadosBusqueda = document.querySelector(".barra_de_busqueda__ingresar_solicitud--resultados");
+
 let contador = 0;
 
 const realizarBusqueda = (nombreBuscado, nombreExistente) => {
     let comprobarEquivalencia = false;
+    const regExLetras = /[a-zñA-ZÑ]/;
+    
     for(let i = 0; i < nombreBuscado.length; i++) {
-        if(nombreBuscado[i] == nombreExistente[i]) {
-            comprobarEquivalencia = true;
+        if(regExLetras.test(nombreBuscado)) {
+            if(nombreBuscado[i].toUpperCase() == nombreExistente[i] || 
+            nombreBuscado[i].toLowerCase() == nombreExistente[i]) {
+                comprobarEquivalencia = true;
+            } else {
+                comprobarEquivalencia = false;
+                break;
+            }
         } else {
-            comprobarEquivalencia = false;
-            break;
+            if(nombreBuscado[i] == nombreExistente[i]) {
+                comprobarEquivalencia = true;
+            } else {
+                comprobarEquivalencia = false;
+                break;
+            }
         }
+        
     }
     if(comprobarEquivalencia) {
         contador++;
         console.log(nombreExistente);
+
+        const productoEncontrado = document.createElement("div");
+        productoEncontrado.classList.add("barra_de_busqueda__ingresar_solicitud--item");
+        productoEncontrado.innerText = nombreExistente;
+        resultadosBusqueda.appendChild(productoEncontrado);
     }
 }
 
@@ -35,5 +55,14 @@ inputBusqueda.addEventListener('keyup', (evento) => {
         contador = 0;
         })
         .catch((error) => alert('Fallo al intentar conectarse con los datos'));
+
+        const anterioresResultados = document.querySelector(".barra_de_busqueda__ingresar_solicitud--resultados").childNodes;
+        
+        console.log(anterioresResultados);
+
+        for(let i = 0; i < anterioresResultados.length; i++) {
+            anterioresResultados[i].remove();
+        }
+
 });
 
